54d79c823235eb92cd006d85882985c11ea4e5d2
judiefriend <105896508+judiefriend@users.noreply.github.com>
2022-10-02 16:44:48 -0600

    Update servers.tf

--- servers.tf
+++ servers.tf
@@ -1,24 +1,31 @@
 resource "aws_instance" "My_First_ec2"{
   ami = data.aws_ami.amazonlinux2.id
-  instance_type = var.instance_type[0]
+  #instance_type = var.instance_type[0]
+  instance_type = var.instance_type_map["prod"]
   subnet_id = aws_subnet.private_a.id
   #vpc_security_group_ids = ["aws_security_group.my_sg.id"]
   key_name = var.key_pair
   user_data = "${file("tomcat.sh")}"
-  tags ={
-    "Name" = "tomcat"
+  count = 3 
+
+  tags = {
+    #"Name" = "tomcat"
+    "Name" = "tomcat-${count.index}"
   }
 }
 
 
 resource "aws_instance" "My_JumpServer"{
   ami = data.aws_ami.amazonlinux2.id
-  instance_type = var.instance_type[1]
+  #instance_type = var.instance_type[1]
+  instance_type = var.instance_type_map["staging"]
   subnet_id = aws_subnet.public_a.id
   #vpc_security_group_ids = ["aws_security_group.my_sg.id"]
   key_name = var.key_pair
   depends_on = [aws_internet_gateway.igw]
-  tags ={
-    "Name" = "JumpServer"
+  count = 2
+
+  tags = {
+    "Name" = "JumpServer-${count.index}"
   }
 }
