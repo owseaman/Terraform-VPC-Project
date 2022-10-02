7526d0146d8e844490954925483cc5ca968b9575
judiefriend <105896508+judiefriend@users.noreply.github.com>
2022-10-02 16:59:38 -0600

    Update VPC sg id

--- servers.tf
+++ servers.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "My_First_ec2"{
   #instance_type = var.instance_type[0]
   instance_type = var.instance_type_map["prod"]
   subnet_id = aws_subnet.private_a.id
-  #vpc_security_group_ids = ["aws_security_group.my_sg.id"]
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
   key_name = var.key_pair
   user_data = "${file("tomcat.sh")}"
   count = 3 
@@ -20,7 +20,7 @@ resource "aws_instance" "My_JumpServer"{
   #instance_type = var.instance_type[1]
   instance_type = var.instance_type_map["staging"]
   subnet_id = aws_subnet.public_a.id
-  #vpc_security_group_ids = ["aws_security_group.my_sg.id"]
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
   key_name = var.key_pair
   depends_on = [aws_internet_gateway.igw]
   count = 2
